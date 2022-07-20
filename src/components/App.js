34c42c1e78aeb277a401907c97fbb88691ccf0b3
Aidana Ualikhan <aidana.ualikhan35@gmail.com>
2022-07-20 20:16:18 +0600

    accordion component complited

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import Accordion from './Accordion';
+
+const items = [
+    {
+        title: 'What is react?',
+        content: 'React is a front and javascript framework',
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favourite framework among engineers',
+    },
+    {
+        title: 'How do you use React?',
+        content: 'Bla bla',
+    },
+];
 
 const App = () => {
-    return <div>Hello, world!</div>;
+    return (
+        <div className='ui container'>
+            <h1>Widgets app</h1>
+            <Accordion items={items} />
+        </div>
+    );
 };
 
 export default App;
