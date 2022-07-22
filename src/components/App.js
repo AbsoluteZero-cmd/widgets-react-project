0a3092499106fb0ccc7a6ab5bf73ea29eb4e1960
Aidana Ualikhan <aidana.ualikhan35@gmail.com>
2022-07-22 18:42:23 +0600

    added header

--- src/components/App.js
+++ src/components/App.js
@@ -35,35 +35,27 @@ const options = [
     },
 ];
 
-const showAccordion = () => {
-    if (window.location.pathname === '/') {
-        return <Accordion items={items} />;
-    }
-};
-const showList = () => {
-    if (window.location.pathname === '/list') {
-        return <Search />;
-    }
-};
-const showDropdown = () => {
-    if (window.location.pathname === '/dropdown') {
-        return <Dropdown options={options} labelText='some label' />;
-    }
-};
-
-const showTranslate = () => {
-    if (window.location.pathname == '/translate') {
-        return <Translate />;
-    }
-};
-
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div className='ui container'>
-            {showAccordion()}
-            {showList()}
-            {showDropdown()}
-            {showTranslate()}
+            <Route path='/'>
+                <Accordion items={items} />
+            </Route>
+            <Route path='/list'>
+                <Search />
+            </Route>
+            <Route path='/dropdown'>
+                <Dropdown
+                    options={options}
+                    labelText='some label'
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path='/translate'>
+                <Translate />
+            </Route>
         </div>
     );
 };
